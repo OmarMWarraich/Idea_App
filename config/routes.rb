878c4eb478fccabb2d6e770426a992ede83646df
OmarMWarraich <earthling986@gmail.com>
2023-01-02 23:37:59 +0500

    fix: Rectify Route for Session Termination

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   resources :comments
   get 'pages/info'
   root to: redirect('/ideas')
-
+  
   resources :ideas
 
 end
